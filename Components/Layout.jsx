21f3e828c7d2f9dcb7b56483976932e9f66705d9
Twinkle-Dot <rmmzale@outlook.com>
2022-07-26 09:50:45 +0300

    added meta data for social media

--- Components/Layout.jsx
+++ Components/Layout.jsx
@@ -7,6 +7,27 @@ const Layout = ({ children }) => {
   return (
     <Fragment>
       <Head>
+        <meta charset="UTF-8" />
+        <meta
+          name="description"
+          content="An open source directory of every country in the world, searchable by name or languages spoken."
+        />
+
+        <meta property="og:title" content="Country Search" />
+        <meta property="og:type" content="web application" />
+        <meta
+          property="og:description"
+          content="An open source country directory."
+        />
+        <meta
+          property="og:image"
+          content="https://i.postimg.cc/VNhy7qp7/Country-Search-Image.png"
+        />
+        <meta
+          property="og:url"
+          content="https://next-country-search.vercel.app/"
+        />
+        <meta name="twitter:card" content="summary_large_image"></meta>
         <title>Country Search</title>
       </Head>
       <Nav />
